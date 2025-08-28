867ebf45e8c4be72705c75e5fc55a43104fc7766
Fiona Y <fyeung21@gmail.com>
2025-08-27 23:29:12 -0700

    add hover styles for cards

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,7 +4,8 @@ export default function Card() {
   return (
     <div className="my-8">
       <h2 className="text-2xl">name of anime</h2>
-      <article className="bg-blue-400 flex justify-end">
+
+      <article className="bg-blue-400 flex justify-between h-50">
         <section>
           <Image
             className="dark:invert"
@@ -15,9 +16,15 @@ export default function Card() {
             priority
           />
         </section>
-        <section>
-          <section>
-            <ul className="pb-8">
+        <section className="group relative flex-col justify-between">
+          <article className="hidden group-hover:block w-40">
+            <p>
+              description Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+              as opposed to using 'Content here, content here'
+            </p>
+          </article>
+          <article className="group-hover:hidden">
+            <ul className="pb-23">
               <li className="text-sm">
                 <p>{"12"} episodes airing in</p>
               </li>
@@ -28,14 +35,14 @@ export default function Card() {
                 <p>by {"name of studio"}</p>
               </li>
             </ul>
-          </section>
-          <section className="flex justify-end space-x-4 ">
+          </article>
+          <article className="flex justify-between space-x-4 justify-self-end">
             <ul className="flex space-x-2">
               <li className="bg-yellow-400">adventure</li>
               <li className="bg-yellow-400">fantasy</li>
             </ul>
             <button className="bg-pink-400">faves</button>
-          </section>
+          </article>
         </section>
       </article>
     </div>
