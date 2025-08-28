a7d3304dc28c00d75413638f2b9573779620262b
Fiona Y <fyeung21@gmail.com>
2025-08-27 23:45:41 -0700

    add comments

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,7 +5,9 @@ export default function Card() {
     <div className="my-8">
       <h2 className="text-2xl">name of anime</h2>
 
+      {/* entire card */}
       <article className="bg-blue-400 flex justify-between h-50">
+        {/* card image side */}
         <section>
           <Image
             className="dark:invert"
@@ -16,13 +18,18 @@ export default function Card() {
             priority
           />
         </section>
+
+        {/* card text side */}
         <section className="group relative flex-col justify-between p-3">
+          {/* card description on hover */}
           <article className="hidden group-hover:block w-41">
             <p>
               description Lorem Ipsum is that it has a more-or-less normal distribution of letters,
               as opposed to using "Content here, content
             </p>
           </article>
+
+          {/* card details */}
           <article className="group-hover:hidden">
             <ul className="pb-20">
               <li className="text-sm">
@@ -36,6 +43,8 @@ export default function Card() {
               </li>
             </ul>
           </article>
+
+          {/* bottom section of card containing genre and favourites button */}
           <article className="flex justify-between space-x-4 justify-self-end">
             <ul className="flex space-x-2">
               <li className="bg-yellow-400">adventure</li>
