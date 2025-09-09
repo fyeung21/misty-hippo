8f0dce0b9c096f445b74449ae694cbb245b83fe9
Fiona Y <fyeung21@gmail.com>
2025-09-09 16:01:38 -0700

    create header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <section className="font-sans min-h-screen m-8 sm:m-20 xl:mx-30">
-          <header>
-            <nav>this will be navigation</nav>
-          </header>
+          <Header/>
           <main>{children}</main>
           <footer>footer</footer>
         </section>
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>this will be navigation</nav>
+    </header>
+  );
+}
