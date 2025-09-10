405aa0ccaac2aae55decfe12667b2c9dc827f60c
Fiona Y <fyeung21@gmail.com>
2025-09-09 17:29:45 -0700

    sketch navigation in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
-      <nav>this will be navigation</nav>
+      <section>
+        <article>
+            <Link href="/">
+              <h1>logo</h1>
+            </Link>
+        </article>
+        <nav>
+          <ul>
+            <li>{`${"winter"} ${"2024"}`}</li>
+            <li>{`${"spring"} ${"2024"}`}</li>
+            <li>{`${"summer"} ${"2024"}`}</li> 
+            <li>{`${"fall"} ${"2024"}`}</li> 
+          </ul> 
+          <ul>
+            <Link href="/favourites">
+              <li>favourites</li>
+            </Link>
+            <Link href="/user">
+              <li>user</li>
+            </Link>
+          </ul>
+        </nav>
+      </section>
     </header>
   );
 }
