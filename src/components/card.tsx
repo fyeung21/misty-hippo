67c79fe72f70c45f588e3d63ddfab9c9731631c8
Fiona Y. <fyeung21@gmail.com>
2025-09-21 22:48:13 -0700

    setup props for card display

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
 
-export default function Card() {
+export type CardProps = {
+  name: string;
+  imgSrc: string;
+  airDate: string;
+  numberEpisodes: number;
+  studioName: string;
+  description: string;
+  genres: string[];
+};
+
+export default function Card({
+  name,
+  imgSrc,
+  airDate,
+  numberEpisodes,
+  studioName,
+  description,
+  genres,
+}: CardProps) {
   return (
     <section>
-      <h2 className="text-2xl">name of anime</h2>
+      <h2 className="text-2xl">{name}</h2>
 
       {/* entire card */}
       <article className="bg-blue-400 flex justify-between h-50">
@@ -11,7 +29,7 @@ export default function Card() {
         <section>
           <Image
             className="dark:invert"
-            src="/next.svg"
+            src={imgSrc}
             alt="Next.js logo"
             width={180}
             height={300}
@@ -23,25 +41,21 @@ export default function Card() {
         <section className="group relative flex-col justify-between p-3">
           {/* card description on hover */}
           <article className="hidden group-hover:block w-41">
-            <p>
-              description Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using "Content here,
-              content
-            </p>
+            <p>{description}</p>
           </article>
 
           {/* card details */}
           <article className="group-hover:hidden">
             <ul className="pb-20">
               <li className="text-sm">
-                <p>{"12"} episodes airing in</p>
+                <p>{numberEpisodes} episodes airing in</p>
               </li>
               <li className="text-xl">
-                <p>October 21, 2025</p>
+                <p>{airDate}</p>
               </li>
               <li>
                 <p>
-                  by <span className="text-blue-500">{"name of studio"}</span>
+                  by <span className="text-blue-500">{studioName}</span>
                 </p>
               </li>
             </ul>
@@ -50,8 +64,11 @@ export default function Card() {
           {/* bottom section of card containing genre and favourites button */}
           <article className="flex justify-between space-x-4 justify-self-end">
             <ul className="flex space-x-2">
-              <li className="bg-yellow-400">adventure</li>
-              <li className="bg-yellow-400">fantasy</li>
+              {genres.map((genre, id) => (
+                <li key={id} className="bg-yellow-400">
+                  {genre}
+                </li>
+              ))}
             </ul>
             <button className="bg-pink-400">faves</button>
           </article>
