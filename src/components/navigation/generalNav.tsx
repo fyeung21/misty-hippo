460b23000f082d404dc8b37bac3f08b8b8df3a8d
Fiona Y. <fyeung21@gmail.com>
2025-09-25 22:47:38 -0700

    add icons to headers

--- src/components/navigation/generalNav.tsx
+++ src/components/navigation/generalNav.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
+import { Heart, User } from "lucide-react";
 
 export default function GeneralNav() {
   return (
     <ul className="flex flex-col text-center capitalize lg:flex-row lg:justify-between lg:bg-indigo-700">
       <Link href="/favourites">
         <li className="p-5 hover:bg-indigo-700 md:mx-2 lg:hover:bg-indigo-800">
-          faves
+          <Heart />
         </li>
       </Link>
       <Link href="/user">
         <li className="p-5 hover:bg-indigo-700 md:ml-2 lg:hover:bg-indigo-800">
-          user
+          <User />
         </li>
       </Link>
     </ul>
