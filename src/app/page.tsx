38a67f2e9d047bad9dc482b987511c0abc3ddfa8
Fiona Y <fyeung21@gmail.com>
2025-10-06 06:16:29 +0000

    clean up comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,6 @@ import CardDetails from "@/services/cardDetails";
 import { MediaItem } from "@/services/cardDetails";
 
 export default async function Home() {
-  // const animeInfo = [
-  //   {
-  //     name: "name of anime",
-  //     imgSrc: "/next.svg",
-  //     airDate: "October 21, 2025",
-  //     numberEpisodes: 12,
-  //     studioName: "studio name",
-  //     description:
-  //       "Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &quote Content here, content &quot;",
-  //     genres: ["adventure", "fantasy"],
-  //   },
-  //   {
-  //     name: "different anime",
-  //     imgSrc: "/next.svg",
-  //     airDate: "December 21, 2025",
-  //     numberEpisodes: 24,
-  //     studioName: "studio name",
-  //     description:
-  //       "Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &quote Content here, content &quot;",
-  //     genres: ["action", "sports"],
-  //   },
-  // ];
-
   const { data } = await CardDetails();
 
   return (
