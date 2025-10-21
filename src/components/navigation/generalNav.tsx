38e7dc92eb8953d12f80b069fbea4148e6397a2e
Fiona Y <fyeung21@gmail.com>
2025-10-21 06:27:40 +0000

    edit mobile hamburger menu styles

--- src/components/navigation/generalNav.tsx
+++ src/components/navigation/generalNav.tsx
@@ -5,13 +5,15 @@ export default function GeneralNav() {
   return (
     <ul className="flex flex-col text-center capitalize lg:flex-row lg:justify-between lg:bg-indigo-700">
       <Link href="/favourites">
-        <li className="p-5 hover:bg-indigo-700 md:mx-2 lg:hover:bg-indigo-800">
-          <Heart />
+        <li className="flex flex-row justify-center p-5 hover:bg-indigo-700 lg:hover:bg-indigo-800">
+          <Heart className="mr-2" />
+          <p>favourites</p>
         </li>
       </Link>
       <Link href="/user">
-        <li className="p-5 hover:bg-indigo-700 md:ml-2 lg:hover:bg-indigo-800">
-          <User />
+        <li className="flex flex-row justify-center p-5 hover:bg-indigo-700 lg:hover:bg-indigo-800">
+          <User className="mr-2" />
+          <p>user</p>
         </li>
       </Link>
     </ul>
